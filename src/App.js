e668f34008d68ca57b671cf333bd894a5139f22c
NeilMathew33 <mathew.neil33@gmail.com>
2026-03-18 19:03:06 -0700

    Job Application Form React Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import JobApplication from './container/JobApplication';
 
 function App() {
   return (
     <div >
-     
+     <JobApplication>
+     </JobApplication>
     </div>
   );
 }
